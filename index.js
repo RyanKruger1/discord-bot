3f24b3cad38e95a823a6cd0e61bcf3ae7d7b559c
RyanKruger1 <ryankruger45@gmail.com>
2021-11-30 15:11:28 +0200

    Create filtering for artciles

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 require('dotenv').config()
 const FeedMe = require('feedme');
 const { link } = require('fs');
@@ -6,40 +7,32 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const Fs = require('fs');
 const CsvReadableStream = require('csv-reader');
-let location = "global"
-let articles = [];
+const now = new Date();
 
-function read() {
-    let counter = 0;
-    https.get('https://www.secjuice.com/rss/', (res) => {
-        if (res.statusCode != 200) {
-            console.error(new Error(`status code ${res.statusCode}`));
-            return;
-        }
-        let parser = new FeedMe();
-        parser.on('title', (title) => {
-            console.log('title of feed is', title);
-        });
-        parser.on('item', (item) => {
-            let newsItem = {
-                t: item.title, l: item.link, d: item.description, p: item.pubdate
-            }
-            articles[counter] = newsItem;
-            counter++;
-
-        });
-        res.pipe(parser);
-    });
-}
 
 function controller(msg) {
     if (msg.content.startsWith("!hello")) {
-        readCSV()
+        postedInTheLastDay("Thu, 25 Nov 2021 16:02:38 +0000");
         msg.reply("world!")
     }
     if (msg.content.startsWith("!display")) {
         display(msg)
     }
+}
+
+function postedInTheLastDay(pubdate) {
+    const dates = pubdate.split(',')[1].split(' ')
+    let day = dates[1].trim()
+    let month = dates[2].trim()
+    let year = dates[3].trim()
+    let time = dates[4].trim()
+    let hour = time.split(':')[0]
+
+    let diffrence = now.getUTCDate(now) - day;
+    if(diffrence <= 1){
+        return true;
+    }
+
 }
 
 function display(msg) {
@@ -63,8 +56,9 @@ function display(msg) {
                         t: item.title, l: item.link, p: item.pubdate
                     }
                     console.log(newsItem);
-                    msg.reply("News\n:"+ item.title +"\n" + item.link+"\n"+item.pubdate)
-
+                    if(postedInTheLastDay(item.pubdate)){
+                        msg.reply("News\n:" + item.title + "\n" + item.link + "\n" + item.pubdate)
+                    }
                 });
                 res.pipe(parser);
             });
